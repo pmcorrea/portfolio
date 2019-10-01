6215c15da756868cbc967dc12b382c021514d035
pmcorrea <pmcorrea@me.com>
2019-09-30 18:33:33 -0600

    Adjusted responsivness, added hamburger stack

---
+++ js.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  $('.menu-contact').toggleClass("hidden");
+  $('.menu-links').toggleClass("hidden");
+}
+
+function hideMenu(x) {
+  $('.menu-container').toggleClass("change");
+  $('.menu-contact').toggleClass("hidden");
+  $('.menu-links').toggleClass("hidden");
+}
