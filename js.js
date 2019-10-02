378c03b32b3ac424b4add8a4c6719e19d4ad78db
pmcorrea <pmcorrea@me.com>
2019-10-02 12:47:34 -0600

    Adjusted nav for larger resolutions

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
-function myFunction(x) {
+// Trigger: Click on hamburger stack
+// Behavior: expands to show nav
+function showNavMenu(x) {
   x.classList.toggle("change");
   $('.menu-contact').toggleClass("hidden");
   $('.menu-links').toggleClass("hidden");
 }
 
+// hides nav menu
 function hideMenu(x) {
   $('.menu-container').toggleClass("change");
   $('.menu-contact').toggleClass("hidden");
