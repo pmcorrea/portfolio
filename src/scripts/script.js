3648b65e65e70d8cc6b58f5f090fcd094f0dc071
pmcorrea <pmcorrea@me.com>
2020-11-09 09:46:32 -0500

    Adjusted for mobile

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,19 @@ let imgIdxCentr = 0;
 let imgIdxDarkLanding = 0;
 let imgIdxStrides = 0;
 
+/* iOS re-orientation fix */
+if (
+	navigator.userAgent.match(/iPhone/i) ||
+	navigator.userAgent.match(/iPad/i)
+) {
+	/* iOS hides Safari address bar */
+	window.addEventListener("load", function () {
+		setTimeout(function () {
+			window.scrollTo(0, 1);
+		}, 1000);
+	});
+}
+
 function changeImg(n, name) {
 	let imgClass;
 	let dotClass;
